0f1e170d3888c898d707be176fabea92dbf2c87e
HJin <guswlsdl7771@naver.com>
2025-06-10 15:03:10 +0900

    init

---
+++ src/main/java/shop/dodream/authapi/AuthApiApplication.java
@@ -0,0 +1,15 @@
+package shop.dodream.authapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class AuthApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthApiApplication.class, args);
+    }
+
+}
