d425ed2553a4f8d69195ca035a92b573f05c1b13
HyunJin <guswlsdl7771@naver.com>
2025-06-13 13:03:50 +0900

    merge: fix/9-login-security into develop (#10)
    refresh시간 7일 -> 1일로 변경
    yml파일 세팅 수정
    jwt인증 에러 표시 출력 변경
    refresh토큰 처음 저장 시 redis에 ttl 하루 세팅
    SecurityConfig 권한 수정
    
    #9

--- src/main/java/shop/dodream/authservice/jwt/JwtAuthenticationEntryPoint.java
+++ src/main/java/shop/dodream/authservice/jwt/JwtAuthenticationEntryPoint.java
@@ -13,6 +13,9 @@ import java.io.IOException;
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write("{\"error\": \"" + authException.getMessage() + "\"}");
     }
 }
