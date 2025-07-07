1cf086621df11561c42894e619d33f6fdd5aa5e8
HJin <guswlsdl7771@naver.com>
2025-07-07 14:36:04 +0900

    feat: 토큰 전달 정보 수정
    - 토큰 전달 정보 수정
    
    #23

--- src/main/java/shop/dodream/authservice/client/UserFeignClient.java
+++ src/main/java/shop/dodream/authservice/client/UserFeignClient.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.*;
 import shop.dodream.authservice.dto.UserResponse;
 import shop.dodream.authservice.dto.payco.PaycoUserRequest;
 
-@FeignClient(name = "user")
+@FeignClient(name = "user",url = "s1.java21.net:10324")
 public interface UserFeignClient {
 
     @GetMapping("/accounts/{user-id}")
---
+++ src/main/java/shop/dodream/authservice/dto/SessionUser.java
@@ -0,0 +1,18 @@
+package shop.dodream.authservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class SessionUser implements Serializable {
+    private String userId;
+    private Role role;
+}
