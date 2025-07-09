1958b54cd92e37da8c305e064381629c57ae0ffb
HyunJin <guswlsdl7771@naver.com>
2025-07-09 16:54:03 +0900

    merge: feat/22-add-swagger into develop (#28)
    - 스웨거 설정 추가
    
    #22

---
+++ src/main/java/shop/dodream/authservice/config/OpenApiConfig.java
@@ -0,0 +1,19 @@
+package shop.dodream.authservice.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI authServiceOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("인증 서비스 API")
+                        .version("v1.0")
+                        .description("회원 인증, JWT 발급을 제공하는 API 문서입니다."));
+    }
+}
